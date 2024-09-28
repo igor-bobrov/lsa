41f3a5da150d76882b4420ea3dbea4d9e4442580
igor-bobrov <i.bobrov.nhaa@gmail.com>
2024-09-28 22:15:19 +0700

    night update 2

--- instalation.sh
+++ instalation.sh
@@ -1,5 +1,4 @@
 sudo pacman -Syu
-
 sudo pacman -Sy viewnior vlc libreoffice-still p7zip nmap veracrypt  os-prober keepassxc thunar virt-manager qemu dnsmasq edk2-ovmf iptables github-cli telegram-desktop neovim man-db xclip htop neofetch numlockx --noconfirm
 # gparted
 sudo pacman -Sy nvidia-dkms nvidia nvidia-utils --noconfirm
@@ -21,22 +20,13 @@ cd
 yay -S librewolf-bin --noconfirm
 yay -Sy Balena Etcher woeusb --noconfirm
 
-cp -r ~/SFurOS/Images ~/Images
-cp -r ~/SFurOS/Books ~/Books
-mkdir -p ~/Documents
-mkdir -p ~/Downloads
-mkdir -p ~/Music
-mkdir -p ~/Videos
-
-chmod +x ~/lsa/SH/add-to-bin
-~/lsa/SH/add-to-bin
 chmod +x ~/lsa/SH/get-chmod
 ~/lsa/SH/get-chmod
 
+~/lsa/SH/det-default-directory
+
 sudo pacman -Rns dolphin --noconfirm
 sudo pacman -Rns vim --noconfirm
-sudo pacman -Rns drun --noconfirm
-
 
 rm ~/.config/dolphinrc
 clear
