4a9072f1df00726868900642935abeb7100eeb7c
igor-bobrov <i.bobrov.nhaa@gmail.com>
2024-10-11 16:29:59 +0700

    half upd

--- instalation.sh
+++ instalation.sh
@@ -1,8 +1,5 @@
 sudo pacman -Syu
 
-
-
-
 sudo pacman -Sy viewnior vlc libreoffice-still p7zip nmap veracrypt  os-prober keepassxc thunar virt-manager qemu dnsmasq edk2-ovmf iptables github-cli telegram-desktop neovim man-db xclip htop neofetch numlockx --noconfirm
 # gparted
 sudo pacman -Sy alsa-utils --noconfirm
@@ -17,6 +14,10 @@ sudo pacman -S freeglut brightnessctl
 sudo pacman -Sy wl-clipboard
 
 sudo pacman -S mako btop
+
+sudo pacman -Sy bluez bluez-utils
+
+
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -36,7 +37,6 @@ yay -S librewolf-bin waypaper --noconfirm
 
 chmod +x ~/lsa/SH/get-chmod
 ~/lsa/SH/get-chmod
-
 ~/lsa/SH/det-default-directory
 ~/lsa/Scrypts/add-cursors
 
