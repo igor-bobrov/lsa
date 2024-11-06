2405edcd52bbe179b291bce036d97d908af2e917
sakari <i.bobrov.nhaa@gmail.com>
2024-11-06 23:37:45 +0700

    bug fix

--- instalation.sh
+++ instalation.sh
@@ -1,10 +1,8 @@
 sudo pacman -Syu
 
-sudo pacman -Sy viewnior vlc libreoffice-still p7zip nmap veracrypt  os-prober keepassxc thunar virt-manager qemu dnsmasq edk2-ovmf iptables github-cli telegram-desktop neovim man-db xclip htop neofetch numlockx --noconfirm
-# gparted
+sudo pacman -Sy viewnior vlc libreoffice-still p7zip nmap veracrypt  os-prober keepassxc thunar virt-manager qemu dnsmasq edk2-ovmf iptables github-cli telegram-desktop neovim man-db xclip neofetch numlockx --noconfirm
 sudo pacman -Sy alsa-utils --noconfirm
-sudo pacman -S mtpfs gvfs-mtp --noconfirm # android
-# hyprpaper
+sudo pacman -S mtpfs gvfs-mtp --noconfirm 
 sudo pacman -Sy waybar hyprland swww rofi --noconfirm
 sudo pacman -Sy hyprcursor hyprlock hyprpicker --noconfirm
 
@@ -14,7 +12,6 @@ sudo pacman -S freeglut brightnessctl
 sudo pacman -Sy wl-clipboard
 
 sudo pacman -S mako btop
-
 sudo pacman -Sy bluez bluez-utils
 
 
@@ -31,18 +28,20 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si --noconfirm
 cd
-
-#yay -S light --noconfirm
 yay -S librewolf-bin waypaper --noconfirm
 
-chmod +x ~/lsa/SH/get-chmod
-~/lsa/SH/get-chmod
-~/lsa/SH/det-default-directory
-~/lsa/Scrypts/add-cursors
+chmod 770 ~/lsa/SH/
+chmod 770 ~/lsa/Scrypts/
+chmod 770 ~/lsa/Scripts/
 
-sudo pacman -Rns dolphin --noconfirm
-sudo pacman -Rns vim htop --noconfirm
+cp -r ~/lsa/cDirectory/Manuals/ ~/
+mkdir -p ~/Documents
+mkdir -p ~/Downloads
+mkdir -p ~/Music
+mkdir -p ~/Videos
 
 rm ~/.config/dolphinrc
 clear
 echo "Done!"
+
+~/lsa/Scripts/update-sys
