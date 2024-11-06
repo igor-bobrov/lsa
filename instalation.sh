5dba7d2531ef39649ad06c54bec8e2c1400ae07b
sakari <i.bobrov.nhaa@gmail.com>
2024-11-07 00:05:55 +0700

    add hyprlock

--- instalation.sh
+++ instalation.sh
@@ -1,20 +1,10 @@
+su
 sudo pacman -Syu
-
-sudo pacman -Sy viewnior vlc libreoffice-still p7zip nmap veracrypt  os-prober keepassxc thunar virt-manager qemu dnsmasq edk2-ovmf iptables github-cli telegram-desktop neovim man-db xclip neofetch numlockx --noconfirm
-sudo pacman -Sy alsa-utils --noconfirm
-sudo pacman -S mtpfs gvfs-mtp --noconfirm 
-sudo pacman -Sy waybar hyprland swww rofi --noconfirm
-sudo pacman -Sy hyprcursor hyprlock hyprpicker --noconfirm
-
-sudo pacman -S ntfs-3g --noconfirm
-
-sudo pacman -S freeglut brightnessctl
-sudo pacman -Sy wl-clipboard
-
-sudo pacman -S mako btop
-sudo pacman -Sy bluez bluez-utils
+chmod 770 ~/lsa/Scripts/*
+sudo ~/lsa/Scripts/install-apps
 
 
+sudo pacman -S ntfs-3g freeglut brightnessctl wl-clipboard mako btop bluez bluez-utils --noconfirm
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -30,9 +20,6 @@ makepkg -si --noconfirm
 cd
 yay -S librewolf-bin waypaper --noconfirm
 
-chmod 770 ~/lsa/SH/
-chmod 770 ~/lsa/Scrypts/
-chmod 770 ~/lsa/Scripts/
 
 cp -r ~/lsa/cDirectory/Manuals/ ~/
 mkdir -p ~/Documents
@@ -45,3 +32,4 @@ clear
 echo "Done!"
 
 ~/lsa/Scripts/update-sys
+reboot
