753afba6a4f03a9921534d6a174053092edcb038
igor-bobrov <i.bobrov.nhaa@gmail.com>
2024-11-09 18:03:14 +0700

    add auto upd

--- instalation.sh
+++ instalation.sh
@@ -1,27 +1,12 @@
 su
 sudo pacman -Syu
 chmod 770 ~/lsa/Scripts/*
-sudo ~/lsa/Scripts/install-apps
-
-
-sudo pacman -S ntfs-3g freeglut brightnessctl wl-clipboard mako btop bluez bluez-utils --noconfirm
-
+sudo ~/lsa/Scripts/NoOdinary/install-apps
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-cd
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si --noconfirm
-cd
-yay -S librewolf-bin waypaper --noconfirm
-
-
 cp -r ~/lsa/cDirectory/Manuals/ ~/
+cp -r ~/lsa/auto-upd ~/
 mkdir -p ~/Documents
 mkdir -p ~/Downloads
 mkdir -p ~/Music
@@ -32,4 +17,4 @@ clear
 echo "Done!"
 
 ~/lsa/Scripts/update-sys
-reboot
+# reboot
